088975383ac62daf4a0422dc8dc7aa3bdf668f55
Marcio Coelho <marcioc@gmail.com>
2020-08-20 22:28:46 -0300

    Ajustes login e logout

--- gpti/gpti/Controllers/HomeController.cs
+++ gpti/gpti/Controllers/HomeController.cs
@@ -8,50 +8,95 @@ using Microsoft.Extensions.Logging;
 using gpti.Models;
 using gpti.ViewModels;
 using gpti.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace gpti.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
         private readonly ICabRepository _cabRepository;
 
-        public HomeController(ICabRepository cabRepository)
+        public HomeController(ICabRepository cabRepository, UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
         {
             _cabRepository = cabRepository;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+                    {
+                        Cab cab = _cabRepository.LerDadosEmpresa();
+                        var homeViewModel = new HomeViewModel
+                        {
+                            Empresa = cab.Empresa,
+                            DadosContato = cab.DadosContato
+                        };
+                        return View("Index", homeViewModel);
+                    }
+                    return RedirectToAction(loginViewModel.ReturnUrl);
+                }
+            }
+
+            ModelState.AddModelError("", "Usuário/senha inválidos ou não localizados");
+            return View(loginViewModel);
         }
 
         public IActionResult Index()
         {
-            Cab cab = _cabRepository.LerDadosEmpresa();
-
-            var homeViewModel = new HomeViewModel
+            var loginViewModel = new LoginViewModel
             {
-                Empresa = cab.Empresa,
-                DadosContato = cab.DadosContato
+
             };
 
-            return View("Login", homeViewModel);
+            return View("Login", loginViewModel);
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
+        //[HttpPost]
+        //public IActionResult GPTI()
+        //{
+        //    Cab cab = _cabRepository.LerDadosEmpresa();
 
-        [HttpPost]
-        public IActionResult GPTI()
-        {
-            Cab cab = _cabRepository.LerDadosEmpresa();
+        //    var homeViewModel = new HomeViewModel
+        //    {
+        //        Empresa = cab.Empresa,
+        //        DadosContato = cab.DadosContato
+        //    };
 
-            var homeViewModel = new HomeViewModel
-            {
-                Empresa = cab.Empresa,
-                DadosContato = cab.DadosContato
-            };
-            
-            return View("Index", homeViewModel);
-        }
+        //    return View("Index", homeViewModel);
+        //}
 
     }
 }
